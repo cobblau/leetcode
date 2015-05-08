b56827d182567458b2bfa31b73f77bdbbfd7d5ed
cobblau <cobblau@gmail.com>
2015-05-09 00:50:05 +0800

    add 136 solution

--- Algorithms/136-single_number.c
+++ Algorithms/136-single_number.c
@@ -1,6 +1,14 @@
 /*
- * Use xor
- */
+Description:
+
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Clue:
+  Use xor
+*/
 
 int singleNumber(int* nums, int numsSize) {
     int ret = 0;
