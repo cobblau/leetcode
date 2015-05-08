a8a92500dffe7920e40ac5e37f0a5035935779fd
元朔 <jinxin.ljx@alibaba-inc.com>
2015-05-09 00:37:46 +0800

    136 solution

---
+++ Algorithms/136-single_number.c
@@ -0,0 +1,13 @@
+/*
+ * Use xor
+ */
+
+int singleNumber(int* nums, int numsSize) {
+    int ret = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        ret ^= nums[i];
+    }
+
+    return ret;
+}
