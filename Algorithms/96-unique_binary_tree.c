8af2b9c809bc66b047d93bb4914cd4a52ea61000
cobblau <cobblau@gmail.com>
2015-05-09 15:17:45 +0800

    add 96's solution

---
+++ Algorithms/96-unique_binary_tree.c
@@ -0,0 +1,28 @@
+/*
+Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+
+For example,
+Given n = 3, there are a total of 5 unique BST's.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+
+ */
+
+int numTrees(int n) {
+    int count[128] = {0};
+
+    count[0] = count[1] = 1;
+    count[2] = 2;
+
+    for (int i = 3; i < 128; i++) {
+        for (int j = 0; j < i; j++) {
+            count[i] += count[j] * count[i - j - 1];
+        }
+    }
+
+    return count[n];
+}
