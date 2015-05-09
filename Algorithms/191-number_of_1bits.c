71b0e66493c91b2e43ce1d6e813e4453da937f86
cobblau <cobblau@gmail.com>
2015-05-09 11:51:38 +0800

    add 191 solution

---
+++ Algorithms/191-number_of_1bits.c
@@ -0,0 +1,16 @@
+/*
+Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+
+For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+ */
+
+int hammingWeight(uint32_t n) {
+    int num = 0;
+
+    for (; n; ) {
+        n = n & (n - 1);
+        num++;
+    }
+
+    return num;
+}
