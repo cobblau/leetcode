a41757277355c27a54ec92a90526bbecd225b28e
cobblau <cobblau@gmail.com>
2015-05-09 11:31:47 +0800

    add 122 solution

---
+++ Algorithms/122-best_time_to_sell_and_buy_stock_II.c
@@ -0,0 +1,19 @@
+/*
+ Description:
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+*/
+
+int maxProfit(int* prices, int pricesSize) {
+    int profit = 0;
+
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1];
+        }
+    }
+
+    return profit;
+}
