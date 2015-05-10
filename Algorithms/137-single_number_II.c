3d2f41b43bc8aa8b5d1e4987a1b51ef4ac8e9abe
cobblau <cobblau@gmail.com>
2015-05-10 18:19:15 +0800

    add 137's solution

---
+++ Algorithms/137-single_number_II.c
@@ -0,0 +1,19 @@
+/*
+Given an array of integers, every element appears three times except for one. Find that single one.
+ */
+
+int singleNumber(int* nums, int numsSize) {
+    int arr[32] = {0};
+    int result = 0;
+
+    for (int i = 0; i < 32; i++) {
+        for (int j = 0; j < numsSize; j++) {
+            if ((nums[j] >> i) & 1)
+                arr[i]++;
+        }
+
+        result |= ((arr[i] % 3) << i);
+    }
+
+    return result;
+}
