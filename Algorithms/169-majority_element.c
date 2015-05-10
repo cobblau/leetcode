92c32d0267a726fca26a92938aca41bf008d2a25
cobblau <cobblau@gmail.com>
2015-05-10 18:46:03 +0800

    add 169's solution

---
+++ Algorithms/169-majority_element.c
@@ -0,0 +1,26 @@
+/*
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+ */
+
+int majorityElement(int* nums, int numsSize) {
+    int candidate;
+    int nTimes;
+
+    candidate = nTimes = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nTimes == 0) {
+            candidate = nums[i];
+            nTimes++;
+        } else {
+            if (candidate == nums[i])
+                nTimes++;
+            else
+                nTimes--;
+        }
+    }
+
+    return candidate;
+}
