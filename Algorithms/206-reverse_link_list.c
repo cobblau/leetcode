cdc09e1e939e9fc7139952615caba3674327fcb4
cobblau <cobblau@gmail.com>
2015-05-10 18:31:16 +0800

    add 206's solution

---
+++ Algorithms/206-reverse_link_list.c
@@ -0,0 +1,29 @@
+/*
+Reverse a singly linked list.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode *p, *q, *t;
+
+    if (head == NULL) return NULL;
+
+    p = head;
+    q = head->next;
+    p->next = NULL;
+
+    while (q) {
+        t = q->next;
+        q->next = p;
+        p = q;
+        q = t;
+    }
+
+    return p;
+}
