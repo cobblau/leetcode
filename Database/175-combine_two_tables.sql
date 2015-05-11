4ee4fc49c61ff381e6cf9c2b35be11e1d6962d77
cobblau <cobblau@gmail.com>
2015-05-12 00:36:43 +0800

    add 175's solution

---
+++ Database/175-combine_two_tables.sql
@@ -0,0 +1,6 @@
+#Question:https://leetcode.com/problems/combine-two-tables/
+#Solution:
+SELECT p.FirstName, p.LastName, a.City, a.State
+FROM Person p
+LEFT JOIN Address a
+ON p.PersonId = a.PersonId
