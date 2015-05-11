a7656f05c101d77edcea69b3193cf5db5a8202fc
cobblau <cobblau@gmail.com>
2015-05-12 01:26:33 +0800

    add 53's solution

---
+++ Algorithms/53-maximum_subarray.c
@@ -0,0 +1,30 @@
+/*
+ Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+    For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+    the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+*/
+
+/*
+
+ Suppose: f[j] is the maximum subarray end with arr[j]
+
+ Then: f[j] = max(f[j-1] + arr[j], arr[j])
+*/
+
+int maxSubArray(int* nums, int numsSize) {
+    int max = 0x80000000;
+    int cur = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        cur += nums[i];
+
+        if (nums[i] > cur)
+            cur = nums[i];
+
+        if (cur > max)
+            max = cur;
+    }
+
+    return max;
+}
