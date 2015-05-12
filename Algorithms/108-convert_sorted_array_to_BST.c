896cb0225b48d436f58f4e701e1c2c982a03c2dc
cobblau <cobblau@gmail.com>
2015-05-13 00:48:32 +0800

    add 108's solution

---
+++ Algorithms/108-convert_sorted_array_to_BST.c
@@ -0,0 +1,26 @@
+/*
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
+    if (numsSize <= 0) {
+        return NULL;
+    }
+
+    struct TreeNode *root = (struct TreeNode*) malloc(sizeof(struct TreeNode));
+    int mid = numsSize / 2;
+    root->val = nums[mid];
+
+    root->left = sortedArrayToBST(nums, mid);
+    root->right = sortedArrayToBST(nums + mid + 1, numsSize - mid - 1);
+
+    return root;
+}
