5c4e828c3b739b107cdd5f082488cbe97ce612d2
cobblau <cobblau@gmail.com>
2015-06-16 00:12:46 +0800

    add 217's solution

---
+++ Algorithms/217-contains_duplicate.c
@@ -0,0 +1,73 @@
+/*
+ * Author: Cobbliu
+ * Email: cobblau@gmail.com
+ */
+
+
+/*
+Description:
+
+    Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+*/
+
+struct Elem {
+    int val;
+    struct Elem *next;
+};
+
+struct Hash {
+    struct Elem **slots;
+    int  cap;
+};
+
+int containsDuplicate(int* nums, int numsSize) {
+    struct Elem *e;
+    int ret = 0;
+
+    struct Hash *set = (struct Hash*) malloc(sizeof(struct Hash));
+
+    set->cap = numsSize * 3 / 4;
+    set->slots = (struct Elem **) calloc(sizeof(struct Elem *), set->cap);
+
+
+    for (int i = 0; i < numsSize; i++) {
+        int index = nums[i] % set->cap;
+
+        if (set->slots[index] == NULL) {
+            e = (struct Elem*) malloc(sizeof(struct Elem));
+            e->val = nums[i];
+            e->next = NULL;
+
+            set->slots[index] = e;
+        } else {
+            for (e = set->slots[index]; e; e = e->next) {
+                if (e->val == nums[i]) {
+                    ret = 1;
+                    goto cleanup;
+                }
+            }
+
+            e = (struct Elem*) malloc(sizeof(struct Elem));
+            e->val = nums[i];
+            e->next = set->slots[index];
+            set->slots[index] = e;
+        }
+    }
+
+ cleanup:
+
+    for (int i = 0; i < set->cap; i++) {
+        struct Elem *tmp = NULL;
+        e = set->slots[i];
+        while (e) {
+            tmp = e;
+            e = e->next;
+            free(tmp);
+        }
+    }
+
+    free(set->slots);
+    free(set);
+
+    return ret;
+}
